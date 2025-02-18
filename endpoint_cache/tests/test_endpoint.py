681a70360b5e57a87c1a8acaeb71e9fa00300bb3
Simone Orsi <simahawk@gmail.com>
2025-02-18 16:20:42 +0100

    endpoint_cache: add pre-heat action

--- endpoint_cache/tests/test_endpoint.py
+++ endpoint_cache/tests/test_endpoint.py
@@ -2,11 +2,16 @@
 # @author: Simone Orsi <simone.orsi@camptocamp.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import base64
+import json
+import textwrap
+
 from freezegun import freeze_time
 
 from odoo import exceptions
 
 from odoo.addons.endpoint.tests.common import CommonEndpoint
+from odoo.addons.website.tools import MockRequest
 
 
 class TestEndpoint(CommonEndpoint):
@@ -98,3 +103,29 @@ class TestEndpoint(CommonEndpoint):
         self.assertFalse(self.endpoint1._endpoint_cache_get("endpoint_cache.test"))
         self.assertFalse(self.endpoint1._endpoint_cache_get("endpoint_cache.test2"))
         self.assertTrue(self.endpoint2._endpoint_cache_get("endpoint_cache.test3"))
+
+    def test_action_preheat_cache(self):
+        self.endpoint1.code_snippet = textwrap.dedent(
+            """
+            cache_name = endpoint._endpoint_cache_make_name("json")
+            cached = endpoint._endpoint_cache_get(cache_name)
+            if cached:
+                result = cached
+            else:
+                result = json.dumps({"foo": "bar"})
+                endpoint._endpoint_cache_store(cache_name, result)
+            resp = Response(result, content_type="application/json", status=200)
+            result = dict(response=resp)
+            """
+        )
+        domain = self.endpoint1._endpoint_view_cache_domain()
+        cache_atts = self.env["ir.attachment"].search(domain)
+        self.assertFalse(cache_atts)
+        with MockRequest(self.env):
+            self.endpoint1.action_preheat_cache()
+        cache_atts = self.env["ir.attachment"].search(domain)
+        self.assertEqual(len(cache_atts), 1)
+        self.assertEqual(cache_atts.mimetype, "application/json")
+        self.assertEqual(
+            json.loads(base64.decodebytes(cache_atts.datas)), {"foo": "bar"}
+        )
