ab5cd1af00d30693109471393cec4c48403fbd12
SilvioC2C <silvio.gregorini@camptocamp.com>
2025-08-19 16:16:48 +0200

    [MIG] endpoint_jsonifier: Migration to 18.0

--- endpoint_jsonifier/__manifest__.py
+++ endpoint_jsonifier/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Endpoint JSONifier",
     "summary": "Allow to configure jsonifier parsers on endpoints",
-    "version": "14.0.1.1.0",
+    "version": "18.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://github.com/OCA/web-api",
     "author": "Camptocamp, Odoo Community Association (OCA)",
